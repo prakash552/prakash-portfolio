3bb5a090d688bc899fa7e7cae050a4d0bd1915ba
Prakash Mishra <ji676349@gmail.com>
2025-07-04 14:51:32 +0530

    contact back

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
   setStatus('');          // ✅ Moved inside function
 
   try {
-   fetch("https://portfolio-backend-6idz.onrender.com/api/contact", {
+    const res = await fetch('https://portfolio-backend-6idz.onrender.com/api/contact', {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({ name, email, message }),
